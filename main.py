9b4885a9626462c8a808418dc5da027fc395e577
Naveen <snaveenkumar343@gmail.com>
2023-01-20 13:14:23 +0530

    average fps

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import cv2
 import time
 
 previous_time = 0
+average_fps = "'/'"
 
 cap = cv2.VideoCapture(0)
 
 avgfps = []
+
+initial_time =time.time()
 
 while True:
     ret, img = cap.read()
@@ -14,11 +17,18 @@ while True:
 
     fps = int(1/(current_time - previous_time))
 
+    avgfps.append(fps)
+
     previous_time = current_time
 
-    avgfps.append(fps)
-    cv2.putText(img, f"FPS : {fps}", (10,25), cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+    timer = time.time() - initial_time
 
+    if timer > 5:
+        average_fps = int(sum(avgfps)/len(avgfps))
+        initial_time = time.time()
+    
+    cv2.putText(img, f"FPS : {fps}", (10,25), cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,0,255),2)
+    cv2.putText(img, f"Avg FPS : {average_fps}", (420,25), cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,0,255),2)
     cv2.imshow("frame",img)
 
     k = cv2.waitKey(1)
